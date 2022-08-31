399e342692ba077dc9001501dad8970c89f7fb16
Jarrod Folino <jarrod.folino@fundingcircle.com>
2022-08-31 12:55:22 +0100

    stuff

--- myarch/apps/sporting_event/models/sporting_event.rb
+++ myarch/apps/sporting_event/models/sporting_event.rb
@@ -10,21 +10,28 @@ class SportingEvent < ActiveRecord::Base
     %i[internal_id name event_date venue location version uuid]
   end
 
-  # def to_h
-  #   %i[internal_id name event_date venue location version uuid].inject({}) do |hash, key|
-  #     hash[key] = send(key)
-  #   end
-  # end
+  def self.sporting_event_created_instance
+    @@sporting_event_created ||=
+      EventPublisher.new('test_exchange', 'sporting_event_created', 'key1')
+  end
 
-  # after_create do
-  #   puts self.class.serialized_attrs.join(', ')
-  #   EventPublisher
-  #     .new('test_exchange', 'sporting_event_created', 'key1')
-  #     .send do
-  #     #   to_json
-  #     '{}'
-  #   end
-  # end
+  after_create do
+    self.class.sporting_event_created_instance
+      .send do
+      self.to_h.to_json
+    end
+  end
+
+  def to_h
+    {
+      internal_id: self.internal_id,
+      name: self.name,
+      event_date: self.event_date,
+      venue: self.venue,
+      version: self.version,
+      uuid: self.uuid
+    }
+  end
 
   def to_s
     "#{self.class} id: #{id}"
--- myarch/lib/ruby/serializable.rb
+++ myarch/lib/ruby/serializable.rb
@@ -2,25 +2,13 @@
 
 module Serializable
   def self.included(base)
-    base.send :include, InstanceMethods
     base.extend ClassMethods
-  end
-
-  module InstanceMethods
-    def as_json(_options = {})
-      self.class.serialized_attrs.inject({}) do |hash, key|
-        hash[key] = send(key)
-      end
-    end
-
-    def to_json(*options)
-      as_json(*options).to_json(*options)
-    end
   end
 
   module ClassMethods
     def from_json(json)
       payloads = JSON.parse(json)
+      payloads = [payloads] unless payloads.instance_of?(Array)
       payloads.map do |payload|
         object = new
         payload.keys { |k, v| object.send("#{k}=", v) if object.respond_to?("#{k}=") }
